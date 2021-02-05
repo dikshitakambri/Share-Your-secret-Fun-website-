8ce95674be52b35820b99ae724a4407eaf01a966
dikshitakambri <dikshitakambri18@gmail.com>
2021-02-06 00:21:58 +0530

    Database encryption

---
+++ app.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true ,useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+/// Database encryption (Level2)
+
+const secret = process.env.SECRET_KEY;
+userSchema.plugin(encrypt, { secret: secret ,encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+
+///register user with username and password (LEVEL 1)
+app.post("/register", function(req, res){
+    const newUser = new User ({
+        email : req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({email :username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
+
+
+// "C:\Program Files\MongoDB\Server\4.4\bin\mongod.exe" --dbpath="c:\data\db"
+
+// "C:\Program Files\MongoDB\Server\4.4\bin\mongo.exe"
