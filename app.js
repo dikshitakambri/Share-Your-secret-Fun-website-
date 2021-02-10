c19acd6660e624ac4549c3f3a8b17ea51892b3d0
dikshitakambri <dikshitakambri18@gmail.com>
2021-02-10 16:04:09 +0530

    database encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -16,6 +17,9 @@ const userSchema = new mongoose.Schema({
     email : String,
     password : String
 });
+
+const secret = process.env.SECRET_KEY;
+userSchema.plugin(encrypt, { secret: secret ,encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
