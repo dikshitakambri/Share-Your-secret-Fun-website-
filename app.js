dce438ac50ec1f89c4a8d2b679549b1fbef0ef59
dikshitakambri <dikshitakambri18@gmail.com>
2021-02-19 16:26:58 +0530

    cookies and session

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -12,14 +13,31 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
     email : String,
     password : String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -33,41 +51,45 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout;
+    res.redirect("/");
+});
 ///register user with username and password (LEVEL 1)
 app.post("/register", function(req, res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User ({
-            email : req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        });
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) { 
+            console.log(err);
+            res.redirect("/register");
+        }else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
     });
-    
 });
 
 app.post("/login", function(req, res){
+    const user = new User ({
+        username : req.body.username,
+        password : req.body.password
+    });
 
-const username = req.body.username;
-const password = (req.body.password);  
-
-    User.findOne({email :username}, function(err, foundUser){
+    req.login(user, function(err){
         if(err){
             console.log(err);
         }else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    result == true;
-                    res.render("secrets");
-                });
-            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         }
     });
 });
@@ -76,8 +98,3 @@ const password = (req.body.password);
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
-
-
-// "C:\Program Files\MongoDB\Server\4.4\bin\mongod.exe" --dbpath="c:\data\db"
-
-// "C:\Program Files\MongoDB\Server\4.4\bin\mongo.exe"
